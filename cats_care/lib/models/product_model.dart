4dda93e0ed01cc9996f794d3dbb8d21a7c69cf72
nhan-ne <2251120370@ut.edu.vn>
2025-04-19 20:14:50 +0700

    Add files via upload

--- cats_care/lib/models/product_model.dart
+++ cats_care/lib/models/product_model.dart
@@ -2,14 +2,33 @@ class ProductModel {
   final String name;
   final String describe;
   final String image;
+  final double price;
+  int quantity;  
 
-  ProductModel({required this.name, required this.describe, required this.image});
+  ProductModel({
+    required this.name,
+    required this.describe,
+    required this.image,
+    required this.price,
+    this.quantity = 1, // Mặc định số lượng là 1
+  });
 
+  // Phương thức factory từ JSON, đảm bảo price là double
   factory ProductModel.fromJson(Map<String, dynamic> json) {
     return ProductModel(
       name: json['name'],
       describe: json['describe'],
       image: json['image'],
+      price: _parsePrice(json['price']), // Gọi hàm xử lý giá
+      quantity: json['quantity'] ?? 1,
     );
+  }
+
+  // Hàm xử lý giá từ chuỗi JSON
+  static double _parsePrice(String? price) {
+    if (price == null) return 0.0;
+    // Loại bỏ các ký tự không phải số và dấu chấm
+    String cleanedPrice = price.replaceAll(RegExp(r'[^\d]'), '');
+    return double.tryParse(cleanedPrice) ?? 0.0;
   }
 }
---
+++ cats_care/lib/models/users_model.dart
@@ -0,0 +1,45 @@
+class UserAddress {
+  final String id;
+  final String name;
+  final String phone;
+  final String address;
+  final String note;
+  final String addressType;
+  final bool isDefault;
+
+  UserAddress({
+    required this.id,
+    required this.name,
+    required this.phone,
+    required this.address,
+    required this.note,
+    required this.addressType,
+    this.isDefault = false,
+  });
+
+  // Chuyển đổi từ Map (Firestore) sang đối tượng
+  factory UserAddress.fromMap(String id, Map<String, dynamic> map) {
+    return UserAddress(
+      id: id,
+      name: map['name'] ?? '',
+      phone: map['phone'] ?? '',
+      address: map['address'] ?? '',
+      note: map['note'] ?? '',
+      addressType: map['addressType'] ?? '',
+      isDefault: map['isDefault'] ?? false,
+
+    );
+  }
+
+  // Chuyển đổi từ đối tượng sang Map (Firestore)
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'phone': phone,
+      'address': address,
+      'note': note,
+      'addressType': addressType,
+      'timestamp': DateTime.now(),
+    };
+  }
+}
